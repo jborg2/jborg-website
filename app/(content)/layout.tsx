8212730ed0f0b350159d82b112e51fe4138a241d
beverm2391 <beverman4528@gmail.com>
2023-05-23 11:08:56 -0400

    fix regular navbar

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -17,18 +17,24 @@ export default function Layout({
             <div className="fixed bottom-0 right-0 mb-4 mr-4">
                 <Toggle />
             </div>
-            <div className='block sm:hidden sticky top-0'>
-                <MobileNavBar />
-            </div>
-            <div className='hidden sm:block w-max'>
-                <NavBar />
-            </div>
             <main className={cn(
                 "flex-1",
-                "min-h-[calc(100vh-4rem)]",
-                // "min-h-screen",
+                // "min-h-[calc(100vh-4rem)]",
+                "min-h-screen",
                 "w-screen",
-            )}>{children}</main>
+            )}>
+                <div
+                    className='max-w-3xl w-full mx-auto'
+                >
+                    {/* <div className='block sm:hidden sticky top-0'>
+                        <MobileNavBar />
+                    </div> */}
+                    <div className='hidden relative sm:block w-max'>
+                        <NavBar />
+                    </div>
+                    {children}
+                </div>
+            </main>
         </div>
     );
 }
